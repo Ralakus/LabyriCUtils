8244d091e57b5c9161fa4a0ca43ea5f370070024
Ralakus <12schneide@gmail.com>
2018-11-07 21:40:36 -0500

    Updated examples

--- examples/example.c
+++ examples/example.c
@@ -1,5 +1,5 @@
-#include "logger.h"
-#include "vector.h"
+#include <lab/logger.h>
+#include <lab/vector.h>
 
 int main() {
 
@@ -8,6 +8,10 @@ int main() {
 
     char hello_world[] = "Hello world!";
     lab_vec_push_back_arr(&hello_str, hello_world, sizeof(hello_world));
+
+    char something[] = "cruel ";
+
+    lab_vec_insert(&hello_str, 6, something, sizeof(something) - 1); // -1 to get rid of null termination
 
     lab_successln("%s", (char*)hello_str.raw_data);
 
