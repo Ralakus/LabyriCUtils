67be86d190a4f6067dcf35d5573ba0acf8cab1cf
Ralakus <12schneide@gmail.com>
2019-04-12 00:21:42 -0400

    premake5 build system support

---
+++ premake5.lua
@@ -0,0 +1,47 @@
+
+is_64_bit = true
+
+workspace "Labyri"
+    language "C"
+    cdialect "C11"
+
+    location "build"
+
+    if is_64_bit then
+        architecture "x86_64"
+    else
+        architecture "x86"
+    end
+
+    configurations { "Debug", "Release" }
+
+    filter { "configurations:Debug" }
+            symbols "On"
+
+    filter { "configurations:Release" }
+            optimize "On"
+    filter { }
+
+    targetdir ("build/bin/")
+
+    objdir ("build/obj/")
+
+
+project "labcutils"
+    kind "StaticLib"
+
+    files { "src/**.h", "src/**.c"}
+
+    if is_64_bit then
+        defines { "LAB_64_BIT" }
+    else 
+        defines { "LAB_32_BIT" }
+    end
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+
+    filter {}
